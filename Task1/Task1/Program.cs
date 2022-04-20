50cb31fd35a8ef616b06d841becfd1fa0e9dbf54
Jamaltscode <jamalts@code.edu.az>
2022-04-20 16:20:51 +0400

    new task

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Task1
+{
+
+    public static class StringExtension
+    {
+        public static string CapitalizeFirst(this string s)
+        {
+            bool IsNewSentense = true;
+
+            string[] str = { "heLlo eVeRYboDy. i aM HeRe" };
+
+            var result = new StringBuilder(str.Length);
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (IsNewSentense && char.IsLetter(s[i]))
+                {
+                    result.Append(char.ToUpper(s[i]));
+                    IsNewSentense = false;
+                }
+                else
+                    result.Append(s[i]);
+
+                if (s[i] == '.' || s[i] == ' ' )
+                {
+                    IsNewSentense = true;
+                    
+                }
+            }
+            
+            return result.ToString();
+
+            Console.WriteLine(result);
+        }
+
+    }
+}
---
+++ Task1/Task1/StringBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task1
+{
+    internal class StringBuilder
+    {
+        private int length;
+
+        public StringBuilder(int length)
+        {
+            this.length = length;
+        }
+
+        internal void Append(char v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
